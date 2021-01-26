c597a5e8d4601206dcd43c8d9f0c67d2415cadbc
John-Holden <johnholden7@gmail.com>
2021-01-26 21:47:38 +0000

    forgot ensemble method

--- run_HPC.py
+++ run_HPC.py
@@ -3,6 +3,7 @@ import datetime
 import numpy as np
 from PARAMETERS_AND_SETUP import Settings
 from runner_methods import parameter_space_iterator
+from ensemble_averaging_methods import run_R0_ensemble
 from ensemble_averaging_methods import mk_new_dir
 
 # - Input variables & setup/save directories
@@ -15,6 +16,6 @@ if job_id == '1':
 # - make directories & run phase
 # ----------------------------------------------------- #
 assert Settings().plot == False
-result = parameter_space_iterator(ensName=ensemble_name, jobId=job_id,
+result = parameter_space_iterator(ensemble_method=run_R0_ensemble, ensName=ensemble_name, jobId=job_id,
                                   N=2, rhos=np.arange(0.002, 0.102, 0.002), betas=np.arange(0.0001, 0.0011, 0.0001))
 
