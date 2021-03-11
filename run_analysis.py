daa1d932869d5582906ccf2e22f71c10452e2513
john.holden11 <john.holden11@nhs.net>
2021-03-11 21:29:27 +0000

    wip refactor landsacpe control package

--- run_analysis.py
+++ run_analysis.py
@@ -7,8 +7,8 @@ from tree_epi_dispersal.ensemble_analysis import process_R0_ensemble
 
 
 if __name__ == '__main__':
-    ens_name = f'{PATH_TO_DATA_STORE}/2021-01-30-hpc-R0-vs-rho'
-    ensemble, rhos, betas = process_R0_ensemble(ens_name, field_of_interest='mean_R0_vs_gen_core_ensemble',
-                                                produce_landscape_control_package=True)
+    ens_name = f'{PATH_TO_DATA_STORE}2021-03-10-hpc-R0-vs-rho'
+    ensemble, rhos, betas = process_R0_ensemble(ens_name, produce_landscape_control_package=True,
+                                                process_nth_gen=3)
 
     plot_rho_beta_ensemble_1D(ensemble, rhos, betas)
